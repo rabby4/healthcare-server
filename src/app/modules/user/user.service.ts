d320c81c22a18361266874b5d74841883e63db40
Rabby <mdrabbybd4747@gmail.com>
2025-05-03 19:56:46 +0600

    create user with admin data

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,24 @@
-const createAdmin = async () => {
-	return {
-		message: "Admin created successfully!",
+import { PrismaClient, UserRole } from "@prisma/client"
+const prisma = new PrismaClient()
+
+const createAdmin = async (data: any) => {
+	const userData = {
+		email: data.admin.email,
+		password: data.password,
+		role: UserRole.ADMIN,
 	}
+
+	const result = await prisma.$transaction(async (transactionClient) => {
+		await transactionClient.user.create({
+			data: userData,
+		})
+		const createdAdminData = await transactionClient.admin.create({
+			data: data.admin,
+		})
+		return createdAdminData
+	})
+
+	return result
 }
 
 export const userServices = {
