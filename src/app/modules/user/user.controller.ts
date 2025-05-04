a9d6e74866660a9b0197c538d1c8f9f5b9627e91
Rabby <mdrabbybd4747@gmail.com>
2025-05-04 12:06:27 +0600

    install bcrypt for hashing users password and add try catch to handle basic error

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,20 @@ import { Request, Response } from "express"
 import { userServices } from "./user.service"
 
 const createAdmin = async (req: Request, res: Response) => {
-	//console.log(req.body)
-	const result = await userServices.createAdmin(req.body)
-	res.send(result)
+	try {
+		const result = await userServices.createAdmin(req.body)
+		res.status(200).json({
+			success: true,
+			message: "Admin created successfully",
+			data: result,
+		})
+	} catch (error: any) {
+		res.status(500).json({
+			success: false,
+			message: error.name || "Something want wrong!",
+			error,
+		})
+	}
 }
 
 export const userController = {
