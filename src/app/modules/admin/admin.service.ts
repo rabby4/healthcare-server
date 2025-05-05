57ca27bd07da51386f28e359af088666ee83f55f
Rabby <mdrabbybd4747@gmail.com>
2025-05-05 19:12:06 +0600

    feat: add filter for admin's other filed

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,21 +3,31 @@ import { Prisma, PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 const getAllAdmin = async (params: any) => {
+	const { searchTerm, ...filterData } = params
 	const andCondition: Prisma.AdminWhereInput[] = []
 
 	const searchFields = ["name", "email"]
 
-	if (params.searchTerm) {
+	if (searchTerm) {
 		andCondition.push({
 			OR: searchFields.map((field) => ({
 				[field]: {
-					contains: params.searchTerm,
+					contains: searchTerm,
 					mode: "insensitive",
 				},
 			})),
 		})
 	}
 
+	if (Object.keys(filterData).length > 0) {
+		andCondition.push({
+			AND: Object.keys(filterData).map((key) => ({
+				[key]: {
+					equals: filterData[key],
+				},
+			})),
+		})
+	}
 	const whereCondition: Prisma.AdminWhereInput = { AND: andCondition }
 
 	const result = await prisma.admin.findMany({
