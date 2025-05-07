77d8e9663231144aa1e85a4d43a0aff325d2590a
Rabby <mdrabbybd4747@gmail.com>
2025-05-07 12:42:49 +0600

    feat(query): add pick function for selecting valid data fields

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express"
 import { adminServices } from "./admin.service"
+import pick from "../../../shared/pick"
 
 const getAllAdmin = async (req: Request, res: Response) => {
+	const filters = pick(req.query, [
+		"name",
+		"email",
+		"searchTerm",
+		"contactNumber",
+	])
 	try {
-		const result = await adminServices.getAllAdmin(req.query)
+		const result = await adminServices.getAllAdmin(filters)
 		res.status(200).json({
 			success: true,
 			message: "All admin fetched successfully!",
