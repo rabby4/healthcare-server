c9d4efeb532d8eddd1139faf848a4574700fdb24
Rabby <mdrabbybd4747@gmail.com>
2025-05-11 01:47:08 +0600

    refactor: refactor router code and install http-status package for show status code on response

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from "express"
 import cors from "cors"
-import { userRoutes } from "./app/modules/user/user.routes"
-import { adminRoutes } from "./app/modules/admin/admin.routes"
+import router from "./app/router"
 
 const app: Application = express()
 
@@ -17,7 +16,6 @@ app.get("/", (req: Request, res: Response) => {
 	})
 })
 
-app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/admin", adminRoutes)
+app.use("/api/v1", router)
 
 export default app
--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -3,6 +3,7 @@ import { adminServices } from "./admin.service"
 import pick from "../../../shared/pick"
 import { adminFilterFields, paginationOptions } from "./admin.constant"
 import sendResponse from "../../../shared/sendResponse"
+import status from "http-status"
 
 const getAllAdmin = async (req: Request, res: Response) => {
 	const filters = pick(req.query, adminFilterFields)
@@ -11,7 +12,7 @@ const getAllAdmin = async (req: Request, res: Response) => {
 	try {
 		const result = await adminServices.getAllAdmin(filters, options)
 		sendResponse(res, {
-			statusCode: 200,
+			statusCode: status.OK,
 			success: true,
 			message: "All admin fetched successfully!",
 			meta: result.meta,
@@ -32,7 +33,7 @@ const getAdminById = async (req: Request, res: Response) => {
 	try {
 		const result = await adminServices.getAdminById(id)
 		sendResponse(res, {
-			statusCode: 200,
+			statusCode: status.OK,
 			success: true,
 			message: "Admin data fetched by id!",
 			data: result,
@@ -52,7 +53,7 @@ const updateAdminById = async (req: Request, res: Response) => {
 	try {
 		const result = await adminServices.updateAdminById(id, data)
 		sendResponse(res, {
-			statusCode: 200,
+			statusCode: status.OK,
 			success: true,
 			message: "Admin data updated!",
 			data: result,
@@ -71,7 +72,7 @@ const deleteAdminById = async (req: Request, res: Response) => {
 	try {
 		const result = await adminServices.deleteAdminById(id)
 		sendResponse(res, {
-			statusCode: 200,
+			statusCode: status.OK,
 			success: true,
 			message: "Admin data deleted!",
 			data: result,
@@ -90,7 +91,7 @@ const softDeleteAdminById = async (req: Request, res: Response) => {
 	try {
 		const result = await adminServices.softDeleteAdminById(id)
 		sendResponse(res, {
-			statusCode: 200,
+			statusCode: status.OK,
 			success: true,
 			message: "Admin data deleted!",
 			data: result,
