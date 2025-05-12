aee69b873e1c3b7b0a841bd27a6041a34a6d2172
Rabby <mdrabbybd4747@gmail.com>
2025-05-12 16:37:20 +0600

    feat(auth): create authentication middleware to checking the user role

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express"
+import { jwtHelpers } from "../../shared/jwtHelper"
+import config from "../../config"
+import { Secret } from "jsonwebtoken"
+
+const auth = (...roles: string[]) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const token = req.headers.authorization
+
+			if (!token) throw new Error("You are not authorized!")
+
+			const verifiedUser = jwtHelpers.verifyToken(
+				token,
+				config.jwt.accessToken as Secret
+			)
+
+			if (roles.length && !roles.includes(verifiedUser.role))
+				throw new Error("You are not authorized!")
+
+			next()
+		} catch (error) {
+			next(error)
+		}
+	}
+}
+
+export default auth
