4fd692928900b7902103207c5715c03ca5b5e7b2
Rabby <mdrabbybd4747@gmail.com>
2025-05-15 19:15:06 +0600

    feat(prisma): add Doctor model to Prisma schema

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -8,7 +8,16 @@ import { userValidation } from "./user.validation"
 const router = express.Router()
 
 router.post(
-	"/",
+	"/create-admin",
+	auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+	fileUploader.upload.single("file"),
+	(req: Request, res: Response, next: NextFunction) => {
+		req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
+		return userController.createAdmin(req, res, next)
+	}
+)
+router.post(
+	"/create-doctor",
 	auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
 	fileUploader.upload.single("file"),
 	(req: Request, res: Response, next: NextFunction) => {
