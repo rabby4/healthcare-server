44358ad03b8b4ec1157618826063fcc730c5b40c
rabby4 <mdrabbybd4747@gmail.com>
2025-07-01 03:25:07 +0600

    feat(server): create exitHandler function to close the server

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,24 @@ const main = async () => {
 	const server: Server = app.listen(config.port, () => {
 		console.log(`App is listening at port ${config.port}`)
 	})
+
+	const exitHandler = () => {
+		if (server) {
+			server.close(() => {
+				console.log("Server is closed due to uncaught exception")
+			})
+		}
+		process.exit(1)
+	}
+
+	process.on("uncaughtException", (error) => {
+		console.log(error)
+		exitHandler()
+	})
+	process.on("unhandledRejection", (error) => {
+		console.log(error)
+		exitHandler()
+	})
 }
 
 main()
